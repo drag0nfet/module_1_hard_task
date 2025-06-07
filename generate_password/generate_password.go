b365cf9ba0701f8be66c9ad121b4b7f0cf6fef6b
sergm <serg.sw03.1@gmail.com>
2025-06-07 15:03:22 +0300

    DONE generate_password

--- generate_password/generate_password.go
+++ generate_password/generate_password.go
@@ -1,10 +1,19 @@
 package generate_password
 
+import (
+	"math/rand"
+)
+
 // letters - список допустимых символов в пароле
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func GeneratePassword(n int) (string, error) {
-	// TODO: ваш код
-
-	panic("implement me")
+	//if n <= 0 {
+	//	return "", errors.New("the length of a password is ought to be positive")
+	//}
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b), nil
 }
