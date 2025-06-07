57be4b1fc222846a6b28aec8ff414164b5dedda0
sergm <serg.sw03.1@gmail.com>
2025-06-07 15:22:32 +0300

    DONE cache

--- cache/cache.go
+++ cache/cache.go
@@ -1,5 +1,7 @@
 package cache
 
+import "sync"
+
 // Interface - реализуйте этот интерфейс
 type Interface interface {
 	Set(k, v string)
@@ -9,21 +11,27 @@ type Interface interface {
 // Не меняйте названия структуры и название метода создания экземпляра Cache, иначе не будут проходить тесты
 
 type Cache struct {
-	// TODO: ваш код
+	storage map[string]string
+	mutex   sync.RWMutex
 }
 
 // NewCache создаёт и возвращает новый экземпляр Cache.
 func NewCache() Interface {
-	// TODO: ваш код
-	panic("implement me")
+	return &Cache{
+		storage: make(map[string]string),
+		mutex:   sync.RWMutex{},
+	}
 }
 
-func (c Cache) Set(k, v string) {
-	// TODO implement me
-	panic("implement me")
+func (c *Cache) Set(k, v string) {
+	defer c.mutex.Unlock()
+	c.mutex.Lock()
+	c.storage[k] = v
 }
 
-func (c Cache) Get(k string) (v string, ok bool) {
-	// TODO implement me
-	panic("implement me")
+func (c *Cache) Get(k string) (v string, ok bool) {
+	defer c.mutex.RUnlock()
+	c.mutex.RLock()
+	v, ok = c.storage[k]
+	return v, ok
 }
